e3dc4586a8f64f64d1bbb61b3c319b29d104d564
Paulius Kukulskis <paukuk@gmail.com>
2015-08-23 21:39:25 +0200

    edited .gitignore file

---
+++ src/components/stringPlay.java
@@ -0,0 +1,11 @@
+package components;
+
+public class stringPlay {
+
+		public static void main(String[] args){
+			
+			StringBuilder string = new StringBuilder("Stringas naujas");
+			System.out.println(string);
+			System.out.println(string.reverse());
+		} 
+}
